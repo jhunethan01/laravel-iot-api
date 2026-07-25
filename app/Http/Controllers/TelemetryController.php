e4046f4e3d66834ef5d6054d1be24aa765b78888
Ethan Lay <jhunethan23@gmail.com>
2026-07-25 21:49:58 +0100

    Adds log to confirm route is now working

--- app/Http/Controllers/TelemetryController.php
+++ app/Http/Controllers/TelemetryController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use Illuminate\Http\Request;
+use Log;
 
 class TelemetryController extends Controller
 {
     public function post(Request $request)
     {
+        // Log::info('Telemetry hit', [
+        //     'payload' => $request->all(),
+        //     'ip' => $request->ip(),
+        // ]);
         return $request->all();
     }
 
